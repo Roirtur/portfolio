c2adb99c58ed4ce5a363f6ecebfc918a9f16e95a
Arthur MACDONALD <arthur.macdonald@etu.u-bordeaux.fr>
2023-06-15 02:49:44 +0200

    New things

--- src/Components/NavigationBar.js
+++ src/Components/NavigationBar.js
@@ -4,12 +4,12 @@ import LanguageContext from './LanguageContext';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faDownload } from '@fortawesome/free-solid-svg-icons';
 
-const CategoriesBox = ({ to, text }) => {
+const CategoriesBox = ({ to, children }) => {
     return (
       <NavLink to={to}>
-        <div className="h-[10%] flex items-center bg-[#1E2022] text-[#F0F5F9] rounded my-5 px-15 shadow-gray-400 shadow-sm duration-300 hover:bg-[#52616B] hover:shadow-[#52616B] hover:drop-shadow-lg">
+        <div className="h-[10%] min-h-[30px] min-w-[120px] flex items-center bg-[#1E2022] text-[#F0F5F9] rounded my-5 px-15 shadow-gray-400 shadow-sm duration-300 hover:bg-[#52616B] hover:shadow-[#52616B] hover:drop-shadow-lg">
           <p className="text-sm p-2 md:text-base md:p-5 lg:text-lg xl:text-xl relative">
-            {text}
+            {children}
           </p>
         </div>
       </NavLink>
@@ -20,28 +20,28 @@ const NavigationBar = () => {
     const { language, toEnglish, toFrench } = React.useContext(LanguageContext);
 
     return (
-        <div class="z-9 font-sans bg-[#F0F5F9] h-screen fixed right-0 w-1/5 shadow-gray-400 shadow-sm p-4">
+        <div class="z-9 font-sans bg-[#F0F5F9] h-screen fixed right-0 w-1/5 shadow-gray-400 shadow-sm p-0 xl:p-4">
             <div class="relative w-full h-full">
                 
-                <div class="h-[5%] w-[30%] bg-[#1E2022] z-10 absolute right-0 top-0 m-5 p-1 mt-1.5 grid grid-cols-2 shadow-gray-400 shadow-sm rounded-full overflow-hidden">
-                    <div onClick={toEnglish} className={`h-full w-full items-center justify-center z-10 pr-[20%] pl-[10%] flex`}>
-                        <img class="pl-[10%]" src="https://img.icons8.com/fluency/48/great-britain-circular.png" alt="great-britain-circular"/>
+                <div class="h-[4%] w-[30%] bg-[#1E2022] min-h-[30px] z-10 absolute right-0 top-0 mr-5 grid grid-cols-2 shadow-gray-400 shadow-sm rounded-full overflow-hidden">
+                    <div onClick={toEnglish} className={`items-center justify-center z-10 pr-[20%] pl-[10%] flex`}>
+                        <img class="pl-[4%] h-[90%] absolute" src="https://img.icons8.com/fluency/48/great-britain-circular.png" alt="great-britain-circular"/>
                     </div>
                     <div onClick={toFrench} className={`items-center justify-center z-10 pl-[20%] pr-[10%] flex`}>
-                        <img class="pr-[10%]" src="https://img.icons8.com/fluency/48/france-circular.png" alt="france-circular"/>
+                        <img class="pr-[4%] h-[90%] absolute" src="https://img.icons8.com/fluency/48/france-circular.png" alt="france-circular"/>
                     </div>
-                    <div className={`absolute bg-[#F0F5F9] border-4 border-[#52616B] w-[55%] h-full transition-all duration-500 rounded-full ${language === "en" ? 'left-0' : 'left-[45%]'}`}/>
+                    <div className={`absolute bg-[#F0F5F9] border-2 border-[#52616B] w-[50%] h-full transition-all duration-500 rounded-full ${language === "en" ? 'left-0' : 'left-[50%]'}`}/>
                 </div>
 
                 <div>
                     <div class="absolute inset-0 p-5 mt-5">
-                        <CategoriesBox to="/" text={language === "en" ? "Home" : "Accueil"}/>
-                        <CategoriesBox to="/profil" text={language === "en" ? "Who am I ?" : "Qui suis-je ?"}/>
-                        <CategoriesBox to="/skills" text={language === "en" ? "Skillset" : "Compétences"}/>
-                        <CategoriesBox to="/portfolio" text="Portfolio"/>
-                        <CategoriesBox to="/situation" text={language === "en" ? "Job situation" : "Situation professionnelle"}/>
-                        <CategoriesBox to="/contact" text={language === "en" ? "Contact me" : "Me contacter"}/>
-                        {/*<CategoriesBox to="/test" text="Test"/>*/}
+                        <CategoriesBox to="/">{language === "en" ? "Home" : "Accueil"}</CategoriesBox>
+                        <CategoriesBox to="/profil">{language === "en" ? "Who am I ?" : "Qui suis-je ?"}</CategoriesBox>
+                        <CategoriesBox to="/skills">{language === "en" ? "Skillset" : "Compétences"}</CategoriesBox>    
+                        <CategoriesBox to="/portfolio">Portfolio</CategoriesBox>
+                        <CategoriesBox to="/situation">{language === "en" ? "Job situation" : "Situation professionnelle"}</CategoriesBox>
+                        <CategoriesBox to="/contact">{language === "en" ? "Contact me" : "Me contacter"}</CategoriesBox>
+                        <CategoriesBox to="/test">Test</CategoriesBox>
                     </div>
                 </div>
                 
@@ -49,7 +49,7 @@ const NavigationBar = () => {
                     <div class="relative flex justify-end">
                         <div class="flex items-center w-full justify-end">
                             <div class="bg-[#1E2022] text-[#F0F5F9] bubble h-10 bounce-animation shadow-gray-400 shadow-sm flex items-center justify-center p-5">
-                                <p class="lg:text-base text-sm text-center">
+                                <p className="text-center text-xs md:text-sm lg:text-base relative">
                                     {language === "en" ? "Download my CV here!" : "Téléchargez mon CV ici !"}
                                 </p>
                             </div>
--- src/Pages/test.js
+++ src/Pages/test.js
@@ -1,8 +1,31 @@
 import React, { useState } from 'react';
 import NavigationBar from '../Components/NavigationBar';
 
+import '../Styles/Test.css';
+
+const Box = ({ w = "200", h = "150", x = "0", y = "0", children }) => {
+  const [shown, setShow] = useState(false);
+  const [clicked, setClicked] = useState(false);
+
+  const act = () => {
+    setClicked(true);
+    setShow(prevState => !prevState);
+  };
+
+  return (
+    <div
+      onClick={act}
+      className={`${
+        clicked ? (shown ? "go-out" : "go-in") : ""
+      } perspectiveBox z-[-${x}] w-[${w}px] h-[${h}px] absolute flex justify-center items-center top-[${y}px] left-[${x}px] bg-[#0000FF] rounded border-4 border-[#FF00FF] drop-shadow-[0_35px_35px_rgba(255,0,255,0.4)]`}
+    >
+      {children}
+    </div>
+  );
+};
+
 const Test = () => {
-  const [slide, setSlide] = useState(false);
+  const [slide, setSlide] = useState(false)
 
   const handleClick = () => {
     setSlide(!slide);
@@ -11,21 +34,22 @@ const Test = () => {
   return (
     <div className="w-full h-10 relative">
       <NavigationBar/>
-      <div className={`h-full absolute inset-0 bg-gray-400 transition-all duration-500 w-1/2 ${slide ? 'left-0' : 'left-1/2'}`}/>
-      <div className="h-full flex items-center justify-center cursor-pointer" onClick={handleClick}>
-      Click me!
-      </div>
-      <div class="box fixed mb-4 mt-1 mx-7 grid grid-cols-2 shadow-gray-400 shadow-sm rounded overflow-hidden">
+      <div class="absolute flex mb-4 mt-1 mx-7 grid grid-cols-2 shadow-gray-400 shadow-sm rounded overflow-hidden">
           <div onClick={handleClick} className={`z-10 px-2 flex text-lg`}>
               EN
           </div>
           <div onClick={handleClick} className={`z-10 px-2 flex text-lg`}>
               FR
           </div>
-          <div className={`absolute bg-gray-400 h-full w-1/2 transition-all duration-500 rounded ${slide ? 'left-0' : 'left-1/2'}`}/>
       </div>
+
+    <Box w="200" h="150" x="300" y="150"> Yo </Box>
+    <Box w="200" h="150" x="200" y="150"> Yo </Box>
+    <Box w="200" h="150" x="250" y="150"> Yo </Box>
+    <Box w="200" h="150" x="150" y="150"> Yo </Box>
+
+
     </div>
-    
   );
 };
 
