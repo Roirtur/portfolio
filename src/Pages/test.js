97bee18d220c7f7ad1595e0ffa10c8b36afd3c92
Arthur MACDONALD <arthur.macdonald@etu.u-bordeaux.fr>
2023-06-15 03:10:44 +0200

    New things

--- src/Pages/test.js
+++ src/Pages/test.js
@@ -3,12 +3,13 @@ import NavigationBar from '../Components/NavigationBar';
 
 import '../Styles/Test.css';
 
-const Box = ({ w = "200", h = "150", x = "0", y = "0", children }) => {
+const Box = ({ w = "200", h = "150", x = "0", y = "0", children , onClick, isShown}) => {
   const [shown, setShow] = useState(false);
   const [clicked, setClicked] = useState(false);
 
   const act = () => {
     setClicked(true);
+    onClick();
     setShow(prevState => !prevState);
   };
 
@@ -16,7 +17,7 @@ const Box = ({ w = "200", h = "150", x = "0", y = "0", children }) => {
     <div
       onClick={act}
       className={`${
-        clicked ? (shown ? "go-out" : "go-in") : ""
+        clicked ? ((isShown && shown) ? "go-out" : "go-in") : ""
       } perspectiveBox z-[-${x}] w-[${w}px] h-[${h}px] absolute flex justify-center items-center top-[${y}px] left-[${x}px] bg-[#0000FF] rounded border-4 border-[#FF00FF] drop-shadow-[0_35px_35px_rgba(255,0,255,0.4)]`}
     >
       {children}
@@ -25,28 +26,24 @@ const Box = ({ w = "200", h = "150", x = "0", y = "0", children }) => {
 };
 
 const Test = () => {
-  const [slide, setSlide] = useState(false)
+  const [shownBoxId, setShownBoxId] = useState(null);
 
-  const handleClick = () => {
-    setSlide(!slide);
+  const handleClick = (boxId) => {
+    if (shownBoxId !== boxId) {
+      setShownBoxId(boxId);
+    }
+    console.log(shownBoxId);
   };
 
   return (
     <div className="w-full h-10 relative">
       <NavigationBar/>
-      <div class="absolute flex mb-4 mt-1 mx-7 grid grid-cols-2 shadow-gray-400 shadow-sm rounded overflow-hidden">
-          <div onClick={handleClick} className={`z-10 px-2 flex text-lg`}>
-              EN
-          </div>
-          <div onClick={handleClick} className={`z-10 px-2 flex text-lg`}>
-              FR
-          </div>
-      </div>
 
-    <Box w="200" h="150" x="300" y="150"> Yo </Box>
-    <Box w="200" h="150" x="200" y="150"> Yo </Box>
-    <Box w="200" h="150" x="250" y="150"> Yo </Box>
-    <Box w="200" h="150" x="150" y="150"> Yo </Box>
+
+      <Box w="200" h="150" x="300" y="150" onClick={() => handleClick('box1')} isShown={shownBoxId === 'box1'}> Yo </Box>
+      <Box w="200" h="150" x="200" y="150" onClick={() => handleClick('box2')} isShown={shownBoxId === 'box2'}> Yo </Box>
+      <Box w="200" h="150" x="250" y="150" onClick={() => handleClick('box3')} isShown={shownBoxId === 'box3'}> Yo </Box>
+      <Box w="200" h="150" x="150" y="150" onClick={() => handleClick('box4')} isShown={shownBoxId === 'box4'}> Yo </Box>
 
 
     </div>
